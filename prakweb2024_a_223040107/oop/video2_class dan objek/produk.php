eb6dc01d96b9a0a5d551e5a2e3f95f6e648432df
humairasyafaira <117334650+humairasyafaira@users.noreply.github.com>
2024-10-13 22:06:37 +0700

    Add files via upload

---
+++ prakweb2024_a_223040107/oop/video2_class dan objek/produk.php
@@ -0,0 +1,14 @@
+<?php
+// Jualan Produk
+// Komik
+// Game
+class Produk {
+    public  $judul,
+            $penulis,
+            $penerbit,
+            $harga;
+
+}
+
+$produk1= new Produk();
+$produk2= new Produk();
