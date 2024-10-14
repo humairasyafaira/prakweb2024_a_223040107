33f2b0a983b63e70fe274f41566d1141f4eda06f
humairasyafaira <117334650+humairasyafaira@users.noreply.github.com>
2024-10-14 08:02:42 +0700

    Delete prakweb2024_a_223040107 directory

--- prakweb2024_a_223040107/mvc/app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('BASEURL', 'http://localhost/prakweb2024_a_223040107/mvc/public');
-
-// DB
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'prakweb2024_a_223040107_pw');
